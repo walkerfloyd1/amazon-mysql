19723502b325dae62e8d363f77196069e0dead42
walkerfloyd1 <walkerkeatley@yahoo.com>
2019-04-15 10:11:57 -0700

    secondcommit

--- bamazon.sql
+++ bamazon.sql
@@ -9,5 +9,42 @@ item_id INT NOT NULL,
 product_name VARCHAR(10),
 department_name VARCHAR(20),
 price INT(50) NULL,
-stock_quantity INT(20) NULL
-)
+stock_quantity INT(20) NULL,
+PRIMARY KEY (item_id)
+);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("Plunger", "Cleaning", "10", 5);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("Snickers", "Food", "1", 20);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("Orange Juice", "Food", "3", 10);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("The Avengers", "Movies", "15", 8);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("Tshirt", "Clothing", "15", 5);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("Football", "Sports", "20", 5);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("TV", "Electronics", "100", 5);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("Xbox", "Electronics", "300", 5);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("Shoes", "Clothing", "30", 6);
+
+INSERT INTO product (product_name, department_name, price, stock_quantity)
+VALUES ("Poster", "Decoration", "5", 3);
+
+
+
+
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,3 +2,58 @@ var sql = require("mysql");
 
 var inquirer = require("inquirer");
 
+var connection = sql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "goheels2!",
+    database: "top_songsDB"
+})
+connection.connect(function(err) {
+    if (err) {
+        console.log(err)
+    };
+    console.log("This is connected");
+    getOrder();
+})
+function getOrder() {
+    inquirer.prompt([{
+        type: "input",
+        name: "idInput",
+        message: "What is the ID of the item?"
+    },
+    {
+        type: "input",
+        name: "quantityInput",
+        message: "How much of the item would you like?"
+    }]).then(function(idInput, quantityInput) {
+        connection.query("SELECT * FROM WHERE ?", {item_id: idInput}, function(err, res) {
+            if (err) {
+                console.log(err)
+            }
+
+            if (res.length === 0) {
+                console.log("Please input an item ID");
+            }
+            else {
+                if (quantityInput > res.stock_quantity) {
+                    console.log("You have exceeded our stock!")
+                }
+                else {
+                    var newQuantity = res.stock_quantity - quantityInput;
+                    connection.query("UPDATE product SET stock_quantity = " + newQuantity + "WHERE item_id = " + idInput, function(err, res) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log("Your order is on the way!")
+                        }
+                    })
+                }
+            }
+        })
+    })
+}
